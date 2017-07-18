059a4d3fd1adce05c3900b3b852baeaf64ed0b39
Blanchard Jérémie <jeremie.blanchard@outlook.fr>
2017-07-18 17:26:31 +0200

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  Button,
+  StyleSheet,
+  View,
+} from 'react-native';
+import { Sentry, SentrySeverity } from 'react-native-sentry';
+
+import Info from '../components/Info';
+
+import { GREETINGS_SCENE_NAME } from '../screens/GreetingsScreen';
+import { JSX_SCENE_NAME } from '../screens/JsxScreen';
+import { STATE_SCENE_NAME } from '../screens/StateScreen';
+
+export const HOME_SCENE_NAME = 'HOME_SCENE';
+//const bug = props.nimp;
+Sentry.config('https://75b00e79f2d24da8b3a8159320760f81:9930cd55a5384f8d950cc29ad678cf34@sentry.io/192862').install();
+
+Sentry.setTagsContext({
+  environment: 'production',
+  react: true,
+});
+
+Sentry.setUserContext({
+  email: 'nhodicq@bewizyu.com',
+  userID: '12341',
+  username: 'nhodicq',
+  extra: {
+    isAdmin: false,
+  },
+});
+
+const styles = StyleSheet.create({
+  margin: {
+    marginTop: 10,
+  },
+});
+
+export default class HomeScreen extends Component {
+
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.navigate = this.props.navigation.navigate;
+
+    this.navigateToGreetings = this.navigateToGreetings.bind(this);
+    this.navigateToJsx = this.navigateToJsx.bind(this);
+    this.navigateToState = this.navigateToState.bind(this);
+  }
+
+  navigateToGreetings() {
+    Sentry.captureMessage('NavigateToGreetings', {
+      level: SentrySeverity.Info,
+    });
+    this.navigate(GREETINGS_SCENE_NAME);
+  }
+
+  navigateToJsx() {
+    this.navigate(JSX_SCENE_NAME);
+  }
+
+  navigateToState() {
+    this.navigate(STATE_SCENE_NAME);
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Info />
+        <View style={styles.margin}>
+          <Button
+            onPress={this.navigateToGreetings}
+            title="Greetings"
+          />
+        </View>
+
+        <View style={styles.margin}>
+          <Button
+            onPress={this.navigateToJsx}
+            title="Jsx"
+          />
+        </View>
+        <View style={styles.margin}>
+          <Button
+            onPress={this.navigateToState}
+            title="State"
+          />
+        </View>
+        <View style={styles.margin}>
+          <Button
+            onPress={() => {
+              throw new Error('Niko test');
+            }}
+            title="Error"
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
